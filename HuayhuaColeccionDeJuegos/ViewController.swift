733dae617a64f10f51e66ab80f25bc320557c6e9
David Alvaro Huayhua Ollachica <levochima@gmail.com>
2023-10-24 17:51:21 -0500

    Initial Commit

---
+++ HuayhuaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HuayhuaColeccionDeJuegos
+//
+//  Created by David Alvaro on 10/24/23.
+//  Copyright © 2023 empresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
